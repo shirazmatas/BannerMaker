3c463547dc7520d8794821be4d88ef174947ae8f
Lucas Lund <lucaslu@kth.se>
2026-05-30 17:53:02 +0200

    Changes by karl

--- src/main/java/club/kid7/bannermaker/BannerMaker.java
+++ src/main/java/club/kid7/bannermaker/BannerMaker.java
@@ -14,6 +14,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
